11c35cb638fded0670dd8e01c57af63c391e941e
Thomas Boittin <tboittin.pro@gmail.com>
2020-09-09 15:41:36 +0200

    fix bs container conflicts

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -9,7 +9,7 @@ import Messages from "../Messages/messages";
 const Chat = ({name, room, messages, message, setMessage, sendMessage}) => {
 
   return (
-    <div className="container">
+    <div className="chat">
       <InfoBar room={room} />
       <Messages 
         messages={messages}
