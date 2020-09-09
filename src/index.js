020facf95741a3f1526671b27c2ea8189d9abd58
Thomas Boittin <tboittin.pro@gmail.com>
2020-09-09 15:41:00 +0200

    add styling for body font-size and bootstrap cdn

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
+
 ReactDOM.render(<App />, document.querySelector('#root'));
