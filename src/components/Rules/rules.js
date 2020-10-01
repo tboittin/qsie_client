6ec3169f5b791635332c91e5cb41ec5d9fd37499
Thomas Boittin <tboittinpro@gmail.com>
2020-10-01 18:15:06 +0200

    Rules 1st design

--- src/components/Rules/rules.js
+++ src/components/Rules/rules.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./rules.scss";
 
 const Rules = () => {
   return (
-    <>
-      <h1>Rules</h1>
-    </>
+    <div className="rules">
+      <h1>Principes</h1>
+      <p>
+        Fille ou garçon? C'est la première question que l'on pose lorsqu'on
+        apprend que l'on va être parent; la première réponse à cocher dans la
+        plupart des questionnaires; le premier choix à faire quand on veut se
+        rendre dans des toilettes publiques.
+      </p>
+      <p>
+        Cette conception binaire du genre commence aujourd'hui à évoluer vers un
+        modèle plus inclusif, mais le changement perne encore à s'imposer comme
+        une évidence.
+      </p>
+      <Link to={`/join`}>
+        <button className="button">J'ai compris les règles</button>
+      </Link>
+    </div>
   );
 };
 
