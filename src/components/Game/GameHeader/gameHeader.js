9e131e68e132dc4b676df35fbe0eb790d8ac4b55
Thomas Boittin <tboittinpro@gmail.com>
2020-10-05 10:27:59 +0200

    gameheader styling

--- src/components/Game/GameHeader/gameHeader.js
+++ src/components/Game/GameHeader/gameHeader.js
@@ -1,42 +1,11 @@
 import React, { useState } from "react";
 
-import "./gameHeader.css";
-import { Row, Col } from "reactstrap";
-import Modal from "reactstrap/lib/Modal";
-import CharacterModal from "../GameGrid/Character/CharacterModal/characterModal";
+import "./gameHeader.scss";
 
-const GameHeader = ({ name, opponentName, userCharacter }) => {
-  const [modal, setModal] = useState(false);
-
-  const toggle = () => setModal(!modal);
-  return (
-    <Row className="game-header m-0">
-      <Col xs="4" className="logo">
-        QUI SONT-IELS?
-      </Col>
-      <Col xs={{ size: "4", offset: "4" }} className="players">
-        <div>
-          <img
-            src={userCharacter.image}
-            alt={userCharacter.name}
-            onClick={toggle}
-          />
-          <h4>{name}</h4>
-        </div>
-        VS
-        <div>
-          ?<h4>{opponentName}</h4>
-        </div>
-      </Col>
-      <Modal>
-        <CharacterModal
-          character={userCharacter}
-          modal={modal}
-          toggle={toggle}
-        />
-      </Modal>
-    </Row>
-  );
-};
+const GameHeader = () => (
+  <div className="game-header">
+    <div className="logo">QUI SONT-IELS?</div>
+  </div>
+);
 
 export default GameHeader;
