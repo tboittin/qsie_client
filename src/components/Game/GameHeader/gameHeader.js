cdfc12c1072b22bb151ad411d9476d58ec39af35
Thomas Boittin <tboittinpro@gmail.com>
2020-10-09 08:16:38 +0200

    retour home depuis game

--- src/components/Game/GameHeader/gameHeader.js
+++ src/components/Game/GameHeader/gameHeader.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import { Modal } from "reactstrap";
 
 import "./gameHeader.scss";
 
-const GameHeader = () => (
-  <div className="game-header">
-    <div className="logo">QUI SONT-IELS?</div>
-  </div>
-);
+const GameHeader = () => {
+  const [home, setHome] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  const goBackToHome = () => {
+    toggle();
+    setHome(true);
+  };
+  return (
+    <>
+      <div className="game-header" onClick={toggle}>
+        <div className="logo hover">QUI SONT-IELS?</div>
+      </div>
+      <Modal isOpen={modal} size="md" centered={true} toggle={toggle}>
+        <div className="modalHeader">
+          <h1>Es-tu sûr de vouloir revenir à l'écran d'accueil ?</h1>
+          <button className="button" onClick={goBackToHome}>
+            Oui, je suis sûr
+          </button>
+          <p className="hover" onClick={toggle}>
+            Non, je veux revenir au jeu
+          </p>
+        </div>
+      </Modal>
+      {home && <Redirect to="/" />}
+    </>
+  );
+};
 
 export default GameHeader;
