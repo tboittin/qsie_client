ef4faacdb92bf17c0f2dc828e8d6a8557a111af7
Thomas Boittin <tboittinpro@gmail.com>
2020-10-15 10:21:27 +0400

    Homepage

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -6,13 +6,21 @@ import "./home.scss";
 const Home = () => {
   return (
     <div className="home">
-      <h1>Homescreen</h1>
-      <Link
-        to={`/rules`}
-      >
-        <button className="button mt-20">
-          Proceed
-        </button>
+      <p className="description">
+        Jeu de déduction, crée en collaboration avec 
+        <span className="refuge"> Le Refuge Genève</span>, Qui Sont-Iels? tente
+        de faire évoluer la discussion sur les stéréotypes de genre et
+        d’orientation.
+      </p>
+      <img className="topLeft" src="./home/QSI_HS_1.png" alt="" />
+      <img className="bottomRight" src="./home/QSI_HS_2.png" alt="" />
+      <img
+        className="logo"
+        src="./home/QSI_HS_Logo.png"
+        alt="Logo Qui Sont-Iels"
+      />
+      <Link to={`/rules`}>
+        <button className="button homeLink">Jouer</button>
       </Link>
     </div>
   );
