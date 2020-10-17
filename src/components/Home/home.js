ce7ff8228a0527261e058878d03fd2b5a6c6e377
Thomas Boittin <tboittinpro@gmail.com>
2020-10-17 10:06:41 +0400

    bouton en construction

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -19,9 +19,10 @@ const Home = () => {
         src="./home/QSI_HS_Logo.png"
         alt="Logo Qui Sont-Iels"
       />
-      <Link to={`/rules`}>
+      {/* <Link to={`/rules`}>
         <button className="button homeLink">Jouer</button>
-      </Link>
+      </Link> */}
+      <p className="homeLink">Le jeu sera disponible très bientôt.</p>
     </div>
   );
 };
