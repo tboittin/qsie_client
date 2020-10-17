715baf99980d0b741397d6b60f8b8d143ef340e4
Thomas Boittin <tboittinpro@gmail.com>
2020-10-17 10:41:57 +0400

    rules modal popup on gameHeader click

--- src/components/Game/GameHeader/gameHeader.js
+++ src/components/Game/GameHeader/gameHeader.js
@@ -1,38 +1,44 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import { Modal } from "reactstrap";
 
 import "./gameHeader.scss";
 
-const GameHeader = ({sendEndGame, changeRoom}) => {
-  const [home, setHome] = useState(false);
+const GameHeader = ({ sendEndGame, changeRoom }) => {
   const [modal, setModal] = useState(false);
 
   const toggle = () => setModal(!modal);
 
-  const goBackToHome = () => {
-    toggle();
-    sendEndGame();
-    changeRoom();
-    setHome(true);
-  };
   return (
     <>
       <div className="game-header" onClick={toggle}>
         <div className="logo hover">QUI SONT-IELS?</div>
       </div>
-      <Modal isOpen={modal} size="md" centered={true} toggle={toggle}>
-        <div className="modalHeader">
-          <h1>Es-tu sûr de vouloir revenir à l'écran d'accueil ?</h1>
-          <button className="button" onClick={goBackToHome}>
-            Oui, je suis sûr
-          </button>
-          <p className="hover" onClick={toggle}>
-            Non, je veux revenir au jeu
+      <Modal isOpen={modal} size="xl" centered={true} toggle={toggle}>
+        <div className="rules-modal">
+          <h1>Principes</h1>
+          <p>
+            Le but du jeu est de découvrir le personnage de ton adversaire en
+            lui posant des questions sur celui-ci.
+          </p>
+          <p>
+            En fonction de ses réponses, tu pourras avancer par déduction
+            jusqu’à n’avoir plus qu’un personnage.
+          </p>
+          <p>
+            Pour pouvoir en savoir plus sur chaque personnage et pour pouvoir
+            les éliminer ou les choisir, il te suffit de cliquer sur chaque
+            image.
+          </p>
+          <p>
+            En fonction de si ton adversaire se trouve à tes côtés ou à
+            distance, tu peux activer ou désactiver le chat.
+          </p>
+          <p>
+            Pour ne pas donner d’indices sur le genre pendant la partie, iel est
+            utilisé comme pronom pour tous les personnages.
           </p>
         </div>
       </Modal>
-      {home && <Redirect to="/" />}
     </>
   );
 };
--- src/components/Rules/rules.js
+++ src/components/Rules/rules.js
@@ -4,36 +4,36 @@ import Circles from "../Circles/circles";
 
 import "./rules.scss";
 
-const Rules = () => {
-  return (
-    <div className="rules">
-      <h1>Principes</h1>
-      <p>
-        Le but du jeu est de découvrir le personnage de ton adversaire en lui
-        posant des questions sur celui-ci.
-      </p>
-      <p>
-        En fonction de ses réponses, tu pourras avancer par déduction jusqu’à
-        n’avoir plus qu’un personnage.
-      </p>
-      <p>
-        Pour pouvoir en savoir plus sur chaque personnage et pour pouvoir les
-        éliminer ou les choisir, il te suffit de cliquer sur chaque image.
-      </p>
-      <p>
-        En fonction de si ton adversaire se trouve à tes côtés ou à distance, tu
-        peux activer ou désactiver le chat.
-      </p>
-      <p>
-        Pour ne pas donner d’indices sur le genre pendant la partie, iel est
-        utilisé comme pronom pour tous les personnages.
-      </p>
+const Rules = (modal) => (
+  <div className={
+    "rules"
+  }>
+    <h1>Principes</h1>
+    <p>
+      Le but du jeu est de découvrir le personnage de ton adversaire en lui
+      posant des questions sur celui-ci.
+    </p>
+    <p>
+      En fonction de ses réponses, tu pourras avancer par déduction jusqu’à
+      n’avoir plus qu’un personnage.
+    </p>
+    <p>
+      Pour pouvoir en savoir plus sur chaque personnage et pour pouvoir les
+      éliminer ou les choisir, il te suffit de cliquer sur chaque image.
+    </p>
+    <p>
+      En fonction de si ton adversaire se trouve à tes côtés ou à distance, tu
+      peux activer ou désactiver le chat.
+    </p>
+    <p>
+      Pour ne pas donner d’indices sur le genre pendant la partie, iel est
+      utilisé comme pronom pour tous les personnages.
+    </p>
       <Link to={`/join`} className="link">
         <button className="button">J'ai compris les règles</button>
       </Link>
-      <Circles numberOfCircles={4} highlitedOne={0} />
-    </div>
-  );
-};
+    <Circles numberOfCircles={4} highlitedOne={0} />
+  </div>
+);
 
 export default Rules;
